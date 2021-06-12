a1d36246795e187ea9bf2fde1f7e3f96f3891983
atgiphy3750 <plastic041@gmail.com>
2021-06-12 18:34:13 +0900

    requestWeather: minor formatting fix

--- src/Utils/requestWeather.ts
+++ src/Utils/requestWeather.ts
@@ -4,7 +4,6 @@ import axios from 'axios';
 const requestWeather = async () => {
 	const url =
 		'https://cors.bridged.cc/http://www.kma.go.kr/wid/queryDFSRSS.jsp?zone=4721062000';
-
 	const response = await axios.get(url);
 	const xml = response.data;
 	const json = parser.parse(xml);
