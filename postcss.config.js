c599c9a684d77e2882ee287ba25ece47ebe65825
atgiphy3750 <plastic041@gmail.com>
2021-06-12 16:58:56 +0900

    css: postcss plugins to support ios 9

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,19 @@
+const tailwindcss = require('tailwindcss');
+const autoprefixer = require('autoprefixer');
+const postcssPresetEnv = require('postcss-preset-env');
+const postcssCalc = require('postcss-calc');
+const postcssCssVariables = require('postcss-css-variables');
+const postcssCustomProperties = require('postcss-custom-properties');
+
 module.exports = {
-	plugins: {
-		tailwindcss: {},
-		autoprefixer: {}
-	}
+	plugins: [
+		tailwindcss(),
+		autoprefixer(),
+		postcssCssVariables(),
+		postcssCustomProperties(),
+		postcssCalc(),
+		postcssPresetEnv({
+			browsers: 'ios 9'
+		})
+	]
 };
