6165c52986acdb19fb21bc56bf9d3599e7594c05
atgiphy3750 <plastic041@gmail.com>
2021-06-12 18:34:33 +0900

    postcss: no require

--- postcss.config.js
+++ postcss.config.js
@@ -1,19 +1,11 @@
-const tailwindcss = require('tailwindcss');
-const autoprefixer = require('autoprefixer');
-const postcssPresetEnv = require('postcss-preset-env');
-const postcssCalc = require('postcss-calc');
-const postcssCssVariables = require('postcss-css-variables');
-const postcssCustomProperties = require('postcss-custom-properties');
-
 module.exports = {
-	plugins: [
-		tailwindcss(),
-		autoprefixer(),
-		postcssCssVariables(),
-		postcssCustomProperties(),
-		postcssCalc(),
-		postcssPresetEnv({
+	plugins: {
+		tailwindcss: {},
+		autoprefixer: {},
+		'postcss-css-variables': {},
+		'postcss-calc': {},
+		'postcss-preset-env': {
 			browsers: 'ios 9'
-		})
-	]
+		}
+	}
 };
