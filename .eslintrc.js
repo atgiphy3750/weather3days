c8cdd3e47074d44bab311dba821c16b548f60c87
atgiphy3750 <plastic041@gmail.com>
2021-06-19 18:51:07 +0900

    format

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,42 +1,42 @@
 module.exports = {
-	env: {
-		browser: true,
-		es2021: true,
-	},
-	plugins: ['prettier', 'react', '@typescript-eslint'],
-	extends: [
-		'xo',
-		'eslint:recommended',
-		'plugin:prettier/recommended',
-		'plugin:react/recommended',
-	],
-	parser: '@typescript-eslint/parser',
-	parserOptions: {
-		ecmaFeatures: {
-			jsx: true,
-		},
-		ecmaVersion: 12,
-		sourceType: 'module',
-	},
-	rules: {
-		'prettier/prettier': [
-			'error',
-			{
-				endOfLine: 'auto',
-			},
-		],
-	},
-	overrides: [
-		{
-			files: ['*.tsx', '*.ts'],
-			rules: {
-				'no-undef': 'off',
-			},
-		},
-	],
-	settings: {
-		react: {
-			version: 'detect',
-		},
-	},
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  plugins: ['prettier', 'react', '@typescript-eslint'],
+  extends: [
+    'xo',
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+    'plugin:react/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+  },
+  overrides: [
+    {
+      files: ['*.tsx', '*.ts'],
+      rules: {
+        'no-undef': 'off',
+      },
+    },
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
 };
